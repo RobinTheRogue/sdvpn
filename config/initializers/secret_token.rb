81e86602bbef988c1ef03e5ec41354df91bfa681
Robin <robin@rogue-coders.com>
2014-02-17 13:50:48 -0700

    initial commit RoR and Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SdVPN::Application.config.secret_key_base = '8e8ef1e7d3b5c8c1637c6370f0e7ab394281c6a37c6ca364dfbbfd4bbe762afed70d4860fd56e527bcc32d2b8c2d5c8021f6285b4414ea3ce570d9dfb2f2ca7e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SdVPN::Application.config.session_store :cookie_store, key: '_sdVPN_session'
